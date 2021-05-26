192e6f8a838cefeecedd0496a55ba304ee3e9e6e
David Young <dcyoung@pobox.com>
2021-05-26 11:58:01 +0100

    WSL path translation (unconditional for now)

--- plugin/core/url.py
+++ plugin/core/url.py
@@ -3,15 +3,19 @@ from urllib.parse import urlparse
 from urllib.request import pathname2url
 from urllib.request import url2pathname
 import os
+import subprocess
 
 
 def filename_to_uri(path: str) -> str:
-    return urljoin('file:', pathname2url(path))
+    # wslpath command doesn't handle unix-side paths correctly (always adds /mnt/c)
+    wsl_path = '/' + '/'.join(path.split('\\')[4:])
+    return urljoin('file:', pathname2url(wsl_path))
 
 
 def uri_to_filename(uri: str) -> str:
     if os.name == 'nt':
         # url2pathname does not understand %3A (VS Code's encoding forced on all servers :/)
-        return url2pathname(urlparse(uri).path).strip('\\')
+        wsl_path = '/' + url2pathname(urlparse(uri).path).strip('\\').replace('\\', '/')
+        return subprocess.check_output(["wsl", "wslpath", "-w", wsl_path], universal_newlines=True, shell=True)[:-1]
     else:
         return url2pathname(urlparse(uri).path)
